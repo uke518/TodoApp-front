b3ff251a8ff13728bfa754e14decc126911f9552
RyomaAdachi <ryomaadaccho6@gmail.com>
2022-09-20 15:57:34 +0900

    編集モーダルのUI作成

--- src/components/UpdateModal.tsx
+++ src/components/UpdateModal.tsx
@@ -2,11 +2,29 @@ import { FC, } from 'react'
 import React from "react";
 import { Todo } from "../types/Todo"
 import { useTodo } from "../hooks/useTodo";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
 
 type Props = {
     todo: Todo
     buttonText: string
     changeIsOpen: (IsOpen: boolean) => void
+}
+
+const defaultProps = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    height: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
 }
 
 export const UpdateModal: FC<Props> = ({ todo, buttonText, changeIsOpen }) => {
@@ -21,26 +39,41 @@ export const UpdateModal: FC<Props> = ({ todo, buttonText, changeIsOpen }) => {
     }
     return (
         <>
-            <form className="InputForm">
-                <div className="Title">
-                    <h1>タイトル</h1>
-                    <input
-                        type="text"
-                        value={title}
-                        onChange={(event) => setTitle(event.target.value)}
-                    />
-                </div>
-                <div className="Content">
-                    <h2>詳細</h2>
-                    <input type="text"
-                        value={content}
-                        onChange={(event) => setContent(event.target.value)}
-                    />
-                </div>
-                <div className="PostForm">
-                    <button type="button" onClick={() => UpdateAndCloseModal()}>{buttonText}</button>
-                    <button type="button" onClick={() => changeIsOpen(false)}>破棄</button>
-                </div>
+            <form>
+                <Box sx={defaultProps}>
+                    <Box m={3}>
+                        <Typography variant="h6" component="h2">
+                            タイトル
+                        </Typography>
+                        <TextField
+                            defaultValue={title}
+                            variant="outlined"
+                            onChange={(event) => setTitle(event.target.value)}
+                            style={{ width: 540 }}
+                        />
+                    </Box>
+                    <Box m={3}>
+                        <Typography>
+                            詳細
+                        </Typography>
+                        <TextField
+                            defaultValue={content}
+                            variant="outlined"
+                            onChange={(event) => setContent(event.target.value)}
+                            style={{ width: 540 }}
+                            multiline
+                            rows={4}
+                        />
+                    </Box>
+                    <Grid container display="flex" justifyContent="flex-end">
+                        <Grid item >
+                            <Button type="button" variant="contained" onClick={() => UpdateAndCloseModal()}>{buttonText}</Button>
+                        </Grid>
+                        <Grid item>
+                            <Button type="button" onClick={() => changeIsOpen(false)}>破棄</Button>
+                        </Grid>
+                    </Grid>
+                </Box>
             </form>
         </>
     );
